ef4f0ee882b70dc8e7a04e59fbf835a784bc7bbb
Luis Leal <luisrrleal@gmail.com>
2025-07-16 11:23:35 -0600

    No limit

--- src/actions/client/get-paginated-client.ts
+++ src/actions/client/get-paginated-client.ts
@@ -4,7 +4,7 @@ import prisma from "@/lib/prisma";
 
 export async function getPaginatedClients(page: number = 1, sectionId?: number, search?: string) {
 
-    const pageSize = 50;
+    const pageSize = 600;
     if (page < 1 || pageSize < 1) {
         page = 1;
     }
